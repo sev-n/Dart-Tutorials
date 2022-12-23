3de8d0814153831a74f4c607c55184e199d3b3e7
sev-n <nielsevilla39@gmail.com>
2022-12-23 10:32:37 +0800

    basics

---
+++ variables.dart
@@ -0,0 +1,25 @@
+void main(){
+
+  // Camel case
+  String firstName;
+  String lastName;
+
+  // Snake case
+  String first_name;
+  String last_name;
+
+  // Pascal case
+  String FirstName;
+  String LastName;
+
+
+  // more
+  String name = "John";
+  int age = 20;
+  print(name);
+
+  // also
+  print("Hello ${name}, ${age} years old");
+  print("Hello $name, $age years old");
+
+}
