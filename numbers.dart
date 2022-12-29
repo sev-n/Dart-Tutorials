7f337405e3e9524ec367155e523b539e51fa8f7f
sev-n <nielsevilla39@gmail.com>
2022-12-29 19:49:19 +0800

    add numbers

---
+++ numbers.dart
@@ -0,0 +1,30 @@
+void main(){
+  
+  // whole numbers
+  int firstNumber = 10;
+  int secondNumber = 30;
+
+  // we can perform arithmetic operation
+
+  print(firstNumber + secondNumber);
+  print(firstNumber - secondNumber);
+  print(firstNumber / secondNumber);
+  print(firstNumber * secondNumber);
+
+  firstNumber++; // increment/ add by 1, same as decrement
+  print(firstNumber);
+
+  firstNumber = 20; // assign new value
+  print(firstNumber); 
+
+  firstNumber = firstNumber + secondNumber; // assign new value by adding first value and second value
+  print(firstNumber);
+
+
+
+
+  
+
+
+
+}
